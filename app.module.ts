03179fc345729c116f653100973f0f46de948ce2
safifarooqui <77325705+safifarooqui@users.noreply.github.com>
2023-06-17 16:07:34 -0500

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
